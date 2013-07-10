ffc7155d6c4356bd5b3609327427137d1eae29d3
CYBUTEK <admin@cybutek.net>
2013-07-11 00:09:52 +0100

    Added click to close & staggered display.

--- TwitchAgent/Notification.cs
+++ TwitchAgent/Notification.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 using System.Diagnostics;
@@ -9,26 +10,52 @@ namespace TwitchAgent
 {
     public partial class Notification : Form
     {
-        private static int _notificationNumber = 0;
+        private static List<Notification> _notificationForms = new List<Notification>();
 
-        private static int NextNotificationNumber()
+        private static int AddForm(Notification form)
         {
-            _notificationNumber++;
-            return _notificationNumber;
+            int indexPosition = 0;
+
+            for (int i = 0; i <= _notificationForms.Count; i++)
+            {
+                if (i == _notificationForms.Count)
+                {
+                    indexPosition = i;
+                    _notificationForms.Add(form);
+                    break;
+                }
+
+                if (_notificationForms[i] == null)
+                {
+                    indexPosition = i;
+                    _notificationForms[i] = form;
+                    break;
+                }
+            }
+
+            return indexPosition;
         }
 
-        private static void PrevNotificationNumber()
+        private static void RemoveForm(Notification form)
         {
-            _notificationNumber--;
+            if (_notificationForms.Contains(form))
+            {
+                _notificationForms[_notificationForms.IndexOf(form)] = null;
+            }
         }
 
+        private int _indexPosition = 0;
         private string _channelName;
 
         public Notification(Channel channel)
         {
-            NextNotificationNumber();
+            _indexPosition = AddForm(this);
 
             InitializeComponent();
+
+            this.MouseUp += FormMouseUp;
+            this.name.MouseUp += FormMouseUp;
+            this.status.MouseClick += FormMouseUp;
 
             _channelName = channel.Name;
             if (channel.Icon != null)
@@ -43,33 +70,44 @@ namespace TwitchAgent
             ThreadManager.StartThread(TimerThread);
         }
 
-
         private void IconClick(object sender, EventArgs e)
         {
             Process.Start("http://www.twitch.tv/" + _channelName);
         }
 
-        private void TimerThread()
+        private void FormMouseUp(object sender, MouseEventArgs e)
         {
-            Stopwatch timer = new Stopwatch();
-            long targetTime = 5000;
-
-            timer.Start();
-            while (!this.IsDisposed && timer.ElapsedMilliseconds < targetTime)
-            {
-                Thread.Sleep(100);
-            }
-
-            this.Invoke((MethodInvoker)delegate
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
             {
                 this.Close();
-            });
+            }
+        }
+
+        private void TimerThread()
+        {
+            try
+            {
+                Stopwatch timer = new Stopwatch();
+                long targetTime = 5000;
+
+                timer.Start();
+                while (!this.IsDisposed && timer.ElapsedMilliseconds < targetTime)
+                {
+                    Thread.Sleep(100);
+                }
+            
+                this.Invoke((MethodInvoker)delegate
+                {
+                    this.Close();
+                });
+            }
+            catch { }
         }
 
         protected override void OnLoad(EventArgs e)
         {
             this.Left = Screen.PrimaryScreen.WorkingArea.Right - this.Width - 2;
-            this.Top = Screen.PrimaryScreen.WorkingArea.Bottom - ((this.Height + 2) * _notificationNumber);
+            this.Top = Screen.PrimaryScreen.WorkingArea.Bottom - ((this.Height + 2) * (_indexPosition + 1));
 
             this.BackgroundImage = Resources.notification_background;
 
@@ -78,8 +116,8 @@ namespace TwitchAgent
 
         protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
         {
-             PrevNotificationNumber();
- 	         base.OnClosing(e);
+            RemoveForm(this);
+ 	        base.OnClosing(e);
         }
 
         protected override bool ShowWithoutActivation { get { return true; } }
