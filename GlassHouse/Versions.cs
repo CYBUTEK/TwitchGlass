63cbdf6bcb20516a930c4fce307a6bfe78c3ba9f
CYBUTEK <admin@cybutek.net>
2013-07-11 03:41:33 +0100

    Fixed versioning.

--- GlassHouse/Versions.cs
+++ GlassHouse/Versions.cs
@@ -2,8 +2,8 @@
 {
     public class Versions
     {
-        public const string GlassHouse = "1.0.0.0";
-        public const string TwitchGlass = "1.0.0.5";
-        public const string TwitchAgent = "1.0.0.2";
+        public const string GlassHouse = "0.1.0.0";
+        public const string TwitchGlass = "0.1.0.5";
+        public const string TwitchAgent = "0.1.0.2";
     }
 }
